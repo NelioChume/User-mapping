0b00bf595727f70e1f648b8f3b5856981e573b7a
NelioChume <74329121+NelioChume@users.noreply.github.com>
2024-03-19 10:15:29 +0200

    conect

--- querys.py
+++ querys.py
@@ -1,22 +1,44 @@
-import conexao_db as conexao
 import psycopg2
+import os
+from dotenv import load_dotenv
+
+# Carregar variáveis de ambiente do arquivo .env
+load_dotenv()
+
+# Variáveis de ambiente para conexão ao banco de dados PostgreSQL
+DB_DATABASE = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+
+def connect():
+    try:
+        # Conectando ao banco de dados
+        conn = psycopg2.connect(dbname=DB_DATABASE, user=DB_USER, password=DB_PASSWORD, host=DB_HOST, port=DB_PORT)
+        print("Conexão bem-sucedida!")
+        return conn
+    except psycopg2.Error as e:
+        print("Erro ao conectar ao PostgreSQL:", e)
 
 def create_database_and_table():
-    conn = conexao.connect()
+    # Conectar-se ao PostgreSQL
+    conn = connect()
     if conn:
         try:
             # Criar um cursor
-            cursor = conn.cursor()
+            cur = conn.cursor()
 
-            # Código SQL para criar a base de dados "mapping_user" e a tabela "authentication_logs"
-            sql_commands = [
-                """
-                CREATE DATABASE IF NOT EXISTS mapping_user;
-                """,
-                """
-                \c mapping_user;
-                """,
-                """
+            # Criar a base de dados "mapping_user" se ainda não existir
+            cur.execute("CREATE DATABASE IF NOT EXISTS mapping_user;")
+            
+            # Conectar-se à base de dados "mapping_user"
+            conn.close()
+            conn = psycopg2.connect(dbname="mapping_user", user=DB_USER, password=DB_PASSWORD, host=DB_HOST, port=DB_PORT)
+            cur = conn.cursor()
+
+            # Criar a tabela "authentication_logs" se ainda não existir
+            cur.execute("""
                 CREATE TABLE IF NOT EXISTS authentication_logs (
                     id SERIAL PRIMARY KEY,
                     container_name VARCHAR(255),
@@ -25,19 +47,15 @@ def create_database_and_table():
                     exception TEXT,
                     datetime TIMESTAMP
                 );
-                """
-            ]
-
-            # Executar os comandos SQL
-            for command in sql_commands:
-                cursor.execute(command)
+            """)
 
             # Commit das alterações
             conn.commit()
 
             # Fechar o cursor e a conexão
-            cursor.close()
+            cur.close()
             conn.close()
+            print("Base de dados e tabela criadas com sucesso!")
         except psycopg2.Error as e:
             print("Erro ao executar comandos SQL:", e)
 
