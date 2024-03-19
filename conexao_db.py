3769939187040edd02c8f3f9340a81f5978a5e0e
NelioChume <74329121+NelioChume@users.noreply.github.com>
2024-03-19 09:43:12 +0200

    port

--- conexao_db.py
+++ conexao_db.py
@@ -9,11 +9,12 @@ DB_HOST = os.getenv("DB_HOST")
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_DATABASE = os.getenv("DB_DATABASE")
+DB_PORT = os.getenv("DB_PORT")
 
 def connect():
     try:
         # Conectando ao banco de dados
-        conn = psycopg2.connect(dbname=DB_DATABASE, user=DB_USER, password=DB_PASSWORD, host=DB_HOST)
+        conn = psycopg2.connect(dbname=DB_DATABASE, user=DB_USER, password=DB_PASSWORD, host=DB_HOST, port=DB_PORT)
         print("Conexão bem-sucedida!")
         return conn
     except psycopg2.Error as e:
